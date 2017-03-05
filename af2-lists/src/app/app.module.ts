4bd2bb9da91e0baa7a822994ad0fbe3c7180d7f8
Kleyson Morais <kleysonb@hotmail.com>
2017-03-05 12:09:41 -0600

    Base Para Aplicação com Firebase

---
+++ af2-lists/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+// Importando AngularFireModule
+import { AngularFireModule } from 'angularfire2';
+
+// AF2 Configurações do AngularFire
+export const firebaseConfig = {
+  apiKey: "AIzaSyDnAX0CQbbsMYuOTJ66ox_F0GwzPM4XPXY",
+  authDomain: "angularfire2-list-example.firebaseapp.com",
+  databaseURL: "https://angularfire2-list-example.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "609067141823"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
